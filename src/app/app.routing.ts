0ea124c384fe2b01f76c70e832b8faff6ce77a4b
pinkal patel <pinkal.patel@vedity.com>
2022-08-18 12:53:34 +0530

    change signinredirect url

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,7 +17,7 @@ export const appRoutes: Route[] = [
     // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
     // path. Below is another redirection for that path to redirect the user to the desired
     // location. This is a small convenience to keep all main routes together here on this file.
-    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'example'},
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'account'},
 
     // Auth routes for guests
     {
